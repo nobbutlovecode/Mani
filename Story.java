fd7577d0e018f622edef88b0f8cf977fd29f5dae
nobbutlovecode <toilanguoivietnam.317@gmail.com>
2026-05-28 21:11:31 +0700

    Create Story.java

---
+++ Story.java
@@ -0,0 +1,14 @@
+package com.mycompany.dacs1.Config;
+
+public class Story {
+    public String id, title, author, category, imageName, description;
+
+    public Story(String id, String title, String author, String category, String imageName, String description) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.imageName = imageName;
+        this.description = description;
+    }
+}

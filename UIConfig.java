32efb0757686467d126295ee1618fda912dbec64
nobbutlovecode <toilanguoivietnam.317@gmail.com>
2026-05-28 21:10:07 +0700

    Create UIConfig.java

---
+++ UIConfig.java
@@ -0,0 +1,10 @@
+package com.mycompany.dacs1.Config;
+import java.awt.Color;
+
+public class UIConfig {
+    public static final Color BG = new Color(245, 247, 250);
+    public static final Color CARD_BG = Color.WHITE;
+    public static final Color PRIMARY = new Color(109, 20, 26); 
+    public static final Color TEXT_MAIN = new Color(44, 62, 80);
+    public static final Color SHADOW = new Color(220, 220, 220);
+}

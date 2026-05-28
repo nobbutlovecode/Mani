6947c82dccf3342c73340fbd00a096d243a277e4
nobbutlovecode <toilanguoivietnam.317@gmail.com>
2026-05-28 21:14:07 +0700

    Create Chapter.java

---
+++ Chapter.java
@@ -0,0 +1,12 @@
+package com.mycompany.dacs1.Config;
+
+public class Chapter {
+    public String chapterId, storyId, chapterName, folderPath;
+
+    public Chapter(String chapterId, String storyId, String chapterName, String folderPath) {
+        this.chapterId = chapterId;
+        this.storyId = storyId;
+        this.chapterName = chapterName;
+        this.folderPath = folderPath;
+    }
+}
